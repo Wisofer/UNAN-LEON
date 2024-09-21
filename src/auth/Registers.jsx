423d0aa24cbbfffeb43cb957efa9fe32cab26f79
wisofer <wisofer17@gmail.com>
2024-09-21 13:25:38 -0600

    Quitando el boton de iniciar seccion con google

--- src/auth/Registers.jsx
+++ src/auth/Registers.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../supabase/supabase';
 
@@ -61,16 +60,6 @@ const Register = () => {
       localStorage.setItem('registrationAttempts', newAttempts.toString());
     } finally {
       setLoading(false);
-    }
-  };
-
-  const handleGoogleRegister = async () => {
-    try {
-      const { error } = await supabase.auth.signIn({ provider: 'google' });
-      if (error) throw error;
-    } catch (error) {
-      console.error("Error durante el registro con Google:", error);
-      setError("Hubo un problema al registrarse con Google. Por favor, inténtalo de nuevo más tarde.");
     }
   };
 
@@ -135,23 +124,6 @@ const Register = () => {
             {loading ? 'Registrando...' : 'Registrarse'}
           </button>
         </form>
-        <div className="mt-4">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300"></div>
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-white text-gray-500">O regístrate con</span>
-            </div>
-          </div>
-          <button
-            onClick={handleGoogleRegister}
-            className="mt-4 w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <FontAwesomeIcon icon={faGoogle} className="mr-2 text-red-500" />
-            Google
-          </button>
-        </div>
         <p className="mt-4 text-center text-sm text-gray-600">
           ¿Ya tienes una cuenta? <button className="text-indigo-600 hover:underline" onClick={() => navigate("/login")}>Inicia sesión aquí</button>
         </p>
